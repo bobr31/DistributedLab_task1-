3ee0330a86fbcb95a8a8c08db5966ac7dbfcd09b
bobr31 <50978420+bobr31@users.noreply.github.com>
2022-05-26 22:47:14 +0300

    My Answer for 1 task
    I make in task 1 paragraph, search how many variations have cod 8x8 16x16 32x32 64x64 and other.

---
+++ DL_1lab/DL_1lab/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Laba3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayIndex arrayIndex = new ArrayIndex();
+
+            arrayIndex.Fields();
+        }
+    }
+    class ArrayData
+    {
+        public int n, k, intn = 1, intk = 1, min, intm = 1, res;
+
+        public void Fields()
+        {
+            Console.Write("Input n:");
+            n = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Input k:");
+            k = Convert.ToInt32(Console.ReadLine());
+
+            for(int i = 1; i <= n; i++)
+            {
+                intn = i * intn;
+            }
+            min = n - k;
+            for (int m = 1; m <= min; m++)
+            {
+                intm = m * intm;
+            }
+            res = intn / (intm);
+            Console.WriteLine(res);
+
+
+        }
+    }
+
+
+    class ArrayIndex : ArrayData
+    {
+        public bool ezz;
+
+        public int masIndex;
+
+
+    }
+}
